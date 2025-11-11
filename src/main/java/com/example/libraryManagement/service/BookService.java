a5da8be6b85a12ce6b887f62d9095051b3adffa9
Carlos <carloswelliton7@gmail.com>
2025-11-11 14:48:49 -0400

    adicionando cache para melhorar o desempenho das requisições

--- src/main/java/com/example/libraryManagement/service/BookService.java
+++ src/main/java/com/example/libraryManagement/service/BookService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.example.libraryManagement.models.Books;
@@ -15,27 +17,33 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
+    @Cacheable("livros_count")
     public Long bookCount(){return bookRepository.count();}
 
+    @Cacheable("livros")
     public List<Books> getAllBooks(){
         return bookRepository.findAll();
     }
 
+    @Cacheable("livrosporid")
     public Optional<Books> getBookById(Long id){
         if(id == null){ throw new RuntimeException("ID do livro não pode ser nulo"); }
         return bookRepository.findById(id);
     }
 
+    @CacheEvict(value={"livros", "livros_count", "livrosporid"}, allEntries = true)
     public Books saveBook(Books book){
         if(book == null){ throw new RuntimeException("As informações do livro estão vazias");}
         return bookRepository.save(book);
     }
 
+    @CacheEvict(value={"livros", "livros_count", "livrosporid"}, allEntries = true)
     public void deleteBook(Long id){
         if(id == null){ throw new RuntimeException("Forneça o ID do livro a ser deletado"); }
         bookRepository.deleteById(id);
     }
 
+    @CacheEvict(value={"livros", "livros_count", "livrosporid"}, allEntries = true)
     public Books updateBook(Books book, Long id){
         if(id == null){ throw new RuntimeException("Fornaça o ID do livro a ser atualizado");}
 
--- src/main/java/com/example/libraryManagement/service/UserService.java
+++ src/main/java/com/example/libraryManagement/service/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.example.libraryManagement.models.Users;
@@ -15,27 +17,33 @@ public class UserService {
     @Autowired
     private UsersRepository userRepository;
 
+    @Cacheable("usuarios_count")
     public Long userCount(){return userRepository.count();}
 
+    @Cacheable("usuarios")
     public List<Users> getAllUsers(){
         return userRepository.findAll();
     }
 
+    @Cacheable(value="usuariospoid", key="#id")
     public Optional<Users> getUserById(Long id){
         if(id == null){ throw new RuntimeException("Usuário não encontrado"); }
         return userRepository.findById(id);
     }
 
+    @CacheEvict(value={"usuarios", "usuarios_count", "usuariospoid"}, allEntries = true)
     public Users saveUser(Users user){
         if(user == null){ throw new RuntimeException("Usuário não pode estar vazio"); }
         return userRepository.save(user);
     }
 
+    @CacheEvict(value={"usuarios", "usuarios_count", "usuariospoid"}, allEntries = true)
     public void deleteUser(Long id){
         if(id == null){ throw new RuntimeException("Usuário não encontrado"); }
         userRepository.deleteById(id);
     }
 
+    @CacheEvict(value={"usuarios", "usuarios_count", "usuariospoid"}, allEntries = true)
     public Users updateUser(Users user, Long id){
         if(id == null){ throw new RuntimeException("Usuário não encontrado para atualizar"); }
 
